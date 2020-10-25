9ed1fb01382a4f526f36530e94fe992cef8b3af5
Efe Bütüner <efebut@gmail.com>
2020-10-25 19:59:40 +0300

    HesapMakinesi.v1

--- EfeButunerHesapMakinesi/Program.cs
+++ EfeButunerHesapMakinesi/Program.cs
@@ -10,6 +10,53 @@ namespace EfeButunerHesapMakinesi
     {
         static void Main(string[] args)
         {
+            double sayi1 = KullaniciGiris("1.sayıyı");
+            double sayi2 = KullaniciGiris("2.sayıyı");
+            double sonuc = 0;
+            char islem = operasyon();
+            sonuc = Hesaplama(sayi1, sayi2, sonuc, islem);
+            Console.WriteLine("Sonuç: " + sonuc);
+            Console.ReadLine();
+        }
+
+        static double Hesaplama(double sayi1, double sayi2, double sonuc, char islem)
+        {
+            switch (islem)
+            {
+                case '+':
+                    sonuc = sayi1 + sayi2;
+                    break;
+
+                case '-':
+                    sonuc = sayi1 - sayi2;
+                    break;
+
+                case '*':
+                    sonuc = sayi1 * sayi2;
+                    break;
+
+                case '/':
+                    sonuc = sayi1 / sayi2;
+                    break;
+
+                default:
+                    Console.WriteLine("Aradığınız işlem bulunamadı.");
+                    break;
+
+            }
+
+            return sonuc;
+        }
+
+        static double KullaniciGiris(string sayıNo)
+        {
+            Console.Write("Lütfen " + sayıNo + " giriniz : ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        static char operasyon()
+        {
+            Console.Write("Lütfen yapmak istediğiniz işlemi giriniz (+,-,*,/): ");
+            return Convert.ToChar(Console.ReadLine());
         }
     }
 }
